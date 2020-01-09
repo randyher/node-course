998058ca3e442652723599935591aa7f2871e305
github username <randyherasme6@gmail.com>
2020-01-09 12:03:18 -0500

    add, remove, list, read added

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,5 +4,61 @@ const validator = require("validator");
 const chalk = require("chalk");
 const yargs = require("yargs");
 
-console.log(process.argv);
-console.log(yargs.argv);
+yargs.version("1.1.0");
+
+//Create Add Command
+yargs.command({
+  command: "add",
+  describe: "Adds a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note content",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function(argv) {
+    console.log(`Title: ${argv.title}`);
+    console.log(`Body: ${argv.body}`);
+  }
+});
+//
+
+//Create Add Command
+yargs.command({
+  command: "remove",
+  describe: "Removes a new note",
+  handler: function() {
+    console.log("Removing a note!");
+  }
+});
+//
+
+//Create Add Command
+yargs.command({
+  command: "list",
+  describe: "List out all note",
+  handler: function() {
+    console.log("List your notes!");
+  }
+});
+//
+
+//Create Add Command
+yargs.command({
+  command: "read",
+  describe: "Reading note",
+  handler: function() {
+    console.log("Read a note!");
+  }
+});
+//
+
+yargs.parse();
+// console.log(process.argv);
+// console.log(yargs.argv);
