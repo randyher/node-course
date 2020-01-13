0db7ea655757e8659148260fe4c3474eb96c3125
github username <randyherasme6@gmail.com>
2020-01-13 09:39:14 -0500

    JSON practice file

--- playground/1-json.js
+++ playground/1-json.js
@@ -6,3 +6,15 @@ const book = {
 
 const bookJSON = JSON.stringify(book);
 fs.writeFileSync("1-json.json", bookJSON);
+
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+
+console.log("Here:", data);
+
+data.title = "A Study in Scarlet";
+data.author = "Arthur Conan Doyle";
+
+const jsonData = JSON.stringify(data);
+fs.writeFileSync("1-json.json", jsonData);
