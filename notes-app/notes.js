e934c2368b7e0ddf3b53f5e3ade98f74a4b97927
github username <randyherasme6@gmail.com>
2020-01-14 10:00:10 -0500

    add note, load note, save note written

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,36 @@
-// getNotes = () => {
-//   return "Your notes...";
-// };
-// module.exports = getNotes;
+const fs = require("fs");
+
+getNotes = () => {
+  return "Your notes...";
+};
+
+const addNote = function(title, body) {
+  const notes = loadNotes();
+  notes.push({
+    title: title,
+    body: body
+  });
+
+  saveNotes(notes);
+  console.log(notes);
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
