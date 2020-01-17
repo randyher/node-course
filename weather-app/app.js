8c11fac0be2de633e70574d9acee6ac25f68c501
github username <randyherasme6@gmail.com>
2020-01-17 15:49:36 -0500

    add req to weather api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
-console.log("Start");
+const request = require("request");
+const url =
+  "https://api.darksky.net/forecast/ddfc0938764a93974f8b2cb846cf2b54/37.8267,-122.4233";
 
-setTimeout(() => {
-  console.log("Han");
-}, 5000);
-console.log("Stop");
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.currently);
+});
