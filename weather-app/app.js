76267994abf89714f2a40ff79f80042d249f5be8
github username <randyherasme6@gmail.com>
2020-01-20 16:15:45 -0500

    clean up weather app code

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,8 +3,7 @@ const url =
   "https://api.darksky.net/forecast/ddfc0938764a93974f8b2cb846cf2b54/37.8267,-122.4233";
 
 request({ url: url, json: true }, (error, response) => {
-  const currently = response.body.currently;
-  const { temperature, precipProbability } = currently;
+  const { temperature, precipProbability } = response.body.currently;
   console.log(
     `It is currently ${temperature} degrees outside. There is a ${precipProbability}% chance of rain`
   );
