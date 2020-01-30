a99fd71d026686de8b0ecbc9e7934fc20e383ec4
github username <randyherasme6@gmail.com>
2020-01-30 11:16:30 -0500

    error handling, fixed output

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,15 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 geocode("Brooklyn", (error, data) => {
-  console.log("Data", data);
-  console.log("Error", error);
-  forecast(data["latitude"], data["longitude"], (error, data) => {
-    console.log("Data", data);
-    console.log("Error", error);
+  if (error) {
+    return console.log(error);
+  }
+  forecast(data["latitude"], data["longitude"], (error, forecastData) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log(data.location);
+    console.log(forecastData);
   });
 });
