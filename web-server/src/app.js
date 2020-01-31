5653b5028f74f4d0af3c9c95542f3925d0fe8b02
github username <randyherasme6@gmail.com>
2020-01-31 11:39:10 -0500

    html files for about and help

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,10 @@
+const path = require("path");
+
 const express = require("express");
 const app = express();
+const publicDirPath = path.join(__dirname, "../public/");
 
-app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send({ name: "Randy", age: 27 });
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About page</h1>");
-});
+app.use(express.static(publicDirPath));
 
 app.get("/weather", (req, res) => {
   res.send({ forecast: "HOT", location: "Brooklyn" });
