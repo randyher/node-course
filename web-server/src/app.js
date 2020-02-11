6ef6f517a4004a3826b62e501ca8f047d827ce5a
github username <randyherasme6@gmail.com>
2020-02-11 14:59:58 -0500

    connected geocode and forecast function

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
 
 // Define Paths for Express Config
 const publicDirPath = path.join(__dirname, "../public/");
@@ -32,7 +34,7 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "Help!",
+    title: "Help",
     name: "Randy"
   });
 });
@@ -42,11 +44,25 @@ app.get("/weather", (req, res) => {
     return res.send({ error: "Require an address" });
   }
 
-  res.send({
-    forecast: "HOT",
-    location: "Brooklyn",
-    address: req.query.address
-  });
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location: location,
+          address: req.query.address
+        });
+      });
+    }
+  );
 });
 
 app.get("/products", (req, res) => {
