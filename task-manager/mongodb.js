4025dcff475a818fc5281a73f026ddf3d327cd2f
github username <randyherasme6@gmail.com>
2020-02-19 14:17:35 -0500

    insert a user to db

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,4 +1,30 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-const
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "taskmanager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect");
+    }
+
+    const db = client.db(databaseName);
+    db.collection("users").insertOne(
+      {
+        name: "Randy",
+        age: 27
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert Users");
+        }
+
+        console.log(result.ops);
+      }
+    );
+  }
+);
