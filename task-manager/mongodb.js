25e02925ef505e6a00d700f36ff662828a5a701e
github username <randyherasme6@gmail.com>
2020-02-20 16:38:27 -0500

    add tasks and destructure mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,4 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "taskmanager";
@@ -13,14 +12,62 @@ MongoClient.connect(
     }
 
     const db = client.db(databaseName);
-    db.collection("users").insertOne(
-      {
-        name: "Randy",
-        age: 27
-      },
+
+    //Insert One Users
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Nicky",
+    //     age: 29
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert Users");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //Insert Many Users
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Alta",
+    //       age: 24
+    //     },
+    //     {
+    //       name: "Regis",
+    //       age: 29
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert users");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //Insert Many Tasks
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Finish tech interview",
+          completed: true
+        },
+        {
+          description: "Intro lecture prep",
+          completed: true
+        },
+        {
+          description: "Go home",
+          completed: false
+        }
+      ],
       (error, result) => {
         if (error) {
-          return console.log("Unable to insert Users");
+          return console.log("Unable to insert users");
         }
 
         console.log(result.ops);
